5ab8944163a1babfbdd68b0942548e072ad6e1f2
= <=>
2023-11-18 11:42:05 -0300

    Functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,78 +1,9 @@
 fun main() {
-    val number: Long = 3_000_000_000
-    println(number)
-
-    //operadores lógicos + - / * boolean(!)
-    //criptografia
-    //char
-
-    val name = "Daniel Moura"
-    print(name.length)
-    print("\n")
-    print(name.uppercase())
-
-
-    val message = "Olá Daniel,\n seu produto chegou!"
-    println(message)
-
-    val welcome = """
-        prod1,
-        numero1!
-        """.replaceIndent(";")
-    println(welcome)
-
-    val nome = "Daniel Moura"
-    //val age = 31
-    //println("Olá, " + nome + ", sua idade é: " +age)
-
-    //println("Olá, $nome, sua idade é: ${age + 10} ")
-
-    val codiName = "Daniel Moura"
-    val age = 31
-    println("Olá, ${codiName} com número de caracteres igual a ${codiName.length}, sua idade é: ${age + 10} ")
-
-    //CONDIÇÕES LÓGICAS
-
-    // E
-    // TRUE | TRUE = true
-    // TRUE | FALSE = false
-    // FALSE | TRUE = false
-    // FALSE | FALSE = false
-
-    // OU
-    // nunca verifica a próxima instrução
-    // TRUE | TRUE = true
-    // TRUE | FALSE = true
-    // FALSE | TRUE = true
-    // FALSE | FALSE = false
-
-    val produto = "Iphone"
-    val preco = 49
-
-    if(preco == 49 || produto == "Iphone") {
-        println("sucesso!")
-    }else {
-        println("false!")
-    }
-
-    //COMPARAR Strings
-    val produto1 = "IMAC"
-    val produto2 = "IMAC"
-    //println(produto1 != produto2) FALSE
-    //prinlln(produto1 == produto2) TRUE
-
-    //
-
-    //aceita null (nullable)
-    val produto3: String? = "Imac"
-
-    //val qntCaracteres = produto3?.length
-    //val qntCaracteres = produto3?.length ?: 0
-
-    if(produto3 != null) {
-        val qntCaracteres = produto3?.length
-        println(qntCaracteres)
-    }
+    //functions
+    soma(10.5, 4.0)
 }
 
+fun soma(num1: Double, num2: Double) {
+    println(num1 + num2)
+}
 
