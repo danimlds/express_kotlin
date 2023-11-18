7e98365fd92e7a75fa1ca7c5c592c1fbe0f74038
= <=>
2023-11-18 12:19:52 -0300

    body inline

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,8 @@
 fun main() {
-    test()
+    //declaração e o corpo da função
+    olaMundo()
 }
+//body inline
+fun olaMundo() = println("Ola Mundo!")
 
-//function dentro de outra function
-fun test() {
-    println("test")
-
-    //funcões de escopo (RARO de usar)
-    fun sub() {
-        println(2 - 1)
-    }
-        sub()
-
-    //fazer várias coisas (login, registro no banco)
-
-        sub()
-}
 
