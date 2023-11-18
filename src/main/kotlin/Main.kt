fcc8409d4de538f1f955fce221588595a45baa4c
= <=>
2023-11-18 12:07:01 -0300

    funções de escopo

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,19 @@
 fun main() {
-    //functions com retornos
-    val resp = soma(10.5, 4.0)
-    println(resp)
+    test()
 }
 
-fun soma(num1: Double, num2: Double): Double{
-    return (num1 + num2)
+//function dentro de outra function
+fun test() {
+    println("test")
+
+    //funcões de escopo (RARO de usar)
+    fun sub() {
+        println(2 - 1)
+    }
+        sub()
+
+    //fazer várias coisas (login, registro no banco)
+
+        sub()
 }
 
