1cc176766b9c1179541b5cf9df672c6c6a396610
= <=>
2023-11-19 18:56:20 -0300

    Parametrizando Funções

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,17 @@
 fun main() {
     //declaração e o corpo da função
     olaMundo()
+    verificarIdade(20)
 }
 //body inline
 fun olaMundo() = println("Ola Mundo!")
+
+fun verificarIdade(age: Int) {
+    if(age > 18) {
+        println("Pode dirigir")
+    }else {
+        println("Não pode dirigir")
+    }
+}
 
 
